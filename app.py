21e396532546fa780590ce922a640cdbfa708222
guser <>
2026-09-17 11:43:48 -0400

    Update app file

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello from Jenkins/ECR!!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Jenkins!"
+
+app.run(host="0.0.0.0", port=5000)
